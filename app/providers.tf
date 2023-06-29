18eddadecd4cc0eaa5ebf17399160be72fb68d1e
Naveen Pusuluri <naveen.pusuluri@avanade.com>
2023-06-29 11:17:09 -0400

    updated

--- app/providers.tf
+++ app/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 3.0.0"
+      version               = ">= 2.0.0"
       /* configuration_aliases = [azurerm.Dev] */
     }
   }
@@ -14,6 +14,6 @@ provider "azurerm" {
   /* alias           = "Dev"
   subscription_id = "871a6abc-8fe8-4427-ae0a-e96b2aced47a"
   tenant_id       = "901ce6e2-eb77-4f3f-a843-3c977e05e4e3" */
-  use_oidc = true
+  
   features {}
 }
